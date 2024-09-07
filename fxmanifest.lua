c02ced34172ac3de8287a560db00df395511b421
Reinier <95980675+reiniervg@users.noreply.github.com>
2024-09-07 23:10:38 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,39 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+files {
+    'html/ui.html',
+    'html/style.css',
+    'html/image.png',
+    'html/pager.png'
+}
+
+ui_page 'html/ui.html'
+
+server_scripts {
+    'server/server.lua'
+}
+
+client_scripts {
+    'client/client.lua',
+    'client/mc_functions.lua',
+    'client/functions.lua',
+    'client/events.lua',
+    'client/commands.lua',
+    'client/warmenu.lua',
+    'client/menu.lua',
+}
+
+shared_scripts {
+    'Config.lua'
+}
+
+escrow_ignore{
+    'config.lua',
+    'client/warmenu.lua'
+}
+
+author 'Team MijnClan Intranet & GMS'
+description 'Dit is de core om de ingame functionaliteiten te verbinden met het MijnClan paneel.'
+version '0.0.5'
